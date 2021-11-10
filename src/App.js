38d6ad8fecd14eecec6c3454bf9693d13ca090cb
Jahanzaib <jahanzaib@clustox.com>
2021-11-10 18:56:16 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import WaterChild from "./WaterChild";
 
 function App() {
+  const [componentArray, setComponentArray] = useState([]);
+  const handlePlus = () => {
+    if (componentArray.length < 5) {
+      setComponentArray((oldArray) => [...oldArray, <WaterChild />]);
+    }
+  };
+  const handleMinus = () => {
+    console.log(componentArray.length);
+    if (componentArray.length > 0) {
+      let tempArray = componentArray;
+      let lengthOfArray = componentArray.length - 1;
+      setComponentArray(tempArray.slice(0, lengthOfArray));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundColor: "#fff",
+      }}
+    >
+      <div style={{ marginBottom: "50px" }}>
+        <button
+          className="button"
+          type="button"
+          style={{ backgroundColor: "#4CAF50" }}
+          onClick={() => handlePlus()}
         >
-          Learn React
-        </a>
-      </header>
+          Fill
+        </button>
+        <button
+          style={{ backgroundColor: "#008CBA" }}
+          className="button"
+          type="button"
+          onClick={() => handleMinus()}
+        >
+          Remove
+        </button>
+      </div>
+      <div
+        className="glass"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+        }}
+      >
+        <div>{componentArray}</div>
+      </div>
     </div>
   );
 }
