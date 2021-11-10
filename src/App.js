f6a33c287f7d87c98dadd1bdcce478675a1fe6bc
Jahanzaib <jahanzaib@clustox.com>
2021-11-10 19:44:10 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -3,43 +3,41 @@ import { useState, useEffect } from "react";
 import WaterChild from "./WaterChild";
 
 function App() {
-  const [componentArray, setComponentArray] = useState([]);
+  const [componentArray, setComponentArray] = useState([<WaterChild />]);
+  const [isDisabledFillBtn, setIsDisabledFillBtn] = useState(false);
+  const [isDisabledRemoveBtn, setIsDisabledRemoveBtn] = useState(true);
   const handlePlus = () => {
     if (componentArray.length < 5) {
       setComponentArray((oldArray) => [...oldArray, <WaterChild />]);
+      setIsDisabledFillBtn(false);
+      setIsDisabledRemoveBtn(true);
     }
   };
   const handleMinus = () => {
-    console.log(componentArray.length);
     if (componentArray.length > 0) {
       let tempArray = componentArray;
       let lengthOfArray = componentArray.length - 1;
       setComponentArray(tempArray.slice(0, lengthOfArray));
+      setIsDisabledRemoveBtn(false);
+      setIsDisabledFillBtn(true);
     }
   };
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundColor: "#fff",
-      }}
-    >
-      <div style={{ marginBottom: "50px" }}>
+    <div className="parentContainer">
+      <div className="mb50">
         <button
           className="button"
           type="button"
-          style={{ backgroundColor: "#4CAF50" }}
+          style={{ backgroundColor: isDisabledFillBtn ? "#275c29" : "#4CAF50" }}
           onClick={() => handlePlus()}
         >
           Fill
         </button>
         <button
-          style={{ backgroundColor: "#008CBA" }}
+          style={{
+            backgroundColor: isDisabledRemoveBtn ? "#326c80" : "#008CBA",
+          }}
           className="button"
           type="button"
           onClick={() => handleMinus()}
